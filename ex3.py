ba1c76e37de95076373a387324627f26a25fdc5e
shiny0129 <165136868+shiny0129@users.noreply.github.com>
2024-04-10 17:07:01 +0800

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,20 @@
+import RPI.GPIO as GPIO
+import time
+
+LED_PIN = [2,3,5,6]
+GPIO.setmode(GPIO.BCM)
+for i in LED_PIN:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, GPIO.HIGH)
+N = eval(input('LOOP NUMBER = '))
+a = N
+while a> 0:
+    print("LOOP %d"%a)
+    for i in LED_PIN:
+        print ('LED %d is ON'%i,end = ', ')
+        GPIO.output(i,GPIO.LOW)
+        time.sleep(0.2)
+        print ("LED %d is OFF"%i)
+        GPIO.output(i,GPIO.HIGH)
+    a=-1
+GPIO.cleanup()
